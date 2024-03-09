34e73621a95d7962364c1b5646ccf41463fd5ee7
Becky Yuen <wyyuen@terpmail.umd.edu>
2024-03-08 20:03:20 -0500

    R code

--- code/HW1.r
+++ code/HW1.r
@@ -13,8 +13,10 @@ names(UID_ISO_FIPS_LookUp_Table)[which(names(UID_ISO_FIPS_LookUp_Table)=="Long_"
 names(time_series_covid19_confirmed_global)[which(names(time_series_covid19_confirmed_global)=="Province/State")] = "Province_State"
 names(time_series_covid19_confirmed_global)[which(names(time_series_covid19_confirmed_global)=="Country/Region")] = "Country_Region"
 
-save(UID_ISO_FIPS_LookUp_Table, file = "UID_ISO_FIPS_LookUp_Table.csv")
-save(time_series_covid19_confirmed_global, file = "time_series_covid19_confirmed_global.csv")
+
+
+save(UID_ISO_FIPS_LookUp_Table, file = "./data/UID_ISO_FIPS_LookUp_Table.csv")
+save(time_series_covid19_confirmed_global, file = "./data/time_series_covid19_confirmed_global.csv")
 
 All_data = merge(time_series_covid19_confirmed_global, UID_ISO_FIPS_LookUp_Table, by = c("Province_State","Country_Region","Lat","Long"))
 
@@ -27,8 +29,8 @@ All_data_long = reshape(data = All_data,
 				 new.row.names = 1:320040,
 				 direction = "long")
 
-save(All_data, file = "All_data_wide.csv")
-save(All_data_long, file = "All_data_long.csv")
+save(All_data, file = "./data/All_data_wide.csv")
+save(All_data_long, file = "./data/All_data_long.csv")
 
 
 library(ggplot2)
@@ -50,7 +52,7 @@ graph1 = ggplot(data = All_data_long_date, mapping = aes(x = date, y = log(ncase
 (fig1 = graph1 + geom_line() + 
 		labs(x = "Time (Date)", y = "Number of cases (in log form)", 
 		title = "Overall change of number of cases (in log form)"))
-ggsave("fig1.jpeg", width = 10, height = 5)
+ggsave("./figures/fig1.jpeg", width = 10, height = 5)
 
 All_data_long_date_country = All_data_long %>% 
 						group_by(date, Country_Region) %>% 
@@ -68,14 +70,14 @@ graph2 = ggplot(data = All_data_long_date_Top5Country, mapping = aes(x = date, y
 (fig2 = graph2 + geom_line(aes(group = Country_Region)) + 
 		labs(x = "Time (Date)", y = "Number of cases (in log form)", 
 		title = "Change of number of cases (in log form) by Country"))
-ggsave("fig2.jpeg", width = 10, height = 5)
+ggsave("./figures/fig2.jpeg", width = 10, height = 5)
 
 
 graph3 = ggplot(data = All_data_long_date_Top5Country, mapping = aes(x = date, y = ncase_bydate/10000))
 (fig3 = graph3 + geom_line(aes(group = Country_Region)) + 
 		labs(x = "Time (Date)", y = "Incident Rate", 
 		title = "Change of Incident Rate (cases per 100,000 persons) by Country"))
-ggsave("fig3.jpeg", width = 10, height = 5)
+ggsave("./figures/fig3.jpeg", width = 10, height = 5)
 
 
 
